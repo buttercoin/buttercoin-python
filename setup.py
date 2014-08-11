893fd3dd8e25d4e449f2b3cfe00d41adbef175cf
Kevin Adams <kevin@buttercoin.com>
2014-08-11 16:38:13 -0700

    removed crypto dependency

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name="buttercoin",
-      version="0.0.1",
+      version="0.0.2",
       description="Python Client for Buttercoin API",
       author="Buttercoin",
       author_email="kevin@buttercoin.com",
       url="https://github.com/buttercoin/buttercoinsdk-python",
       packages=["buttercoin"],
-      install_requires=["requests", "pycrypto", "jsonurl"],
+      install_requires=["requests", "jsonurl"],
       tests_require=["nose"]
 )
